2b260e410fd1576377b0f7b94a8a5f8161acb1d4
raphthtech <raphaellthomas@gmail.com>
2022-06-28 09:07:27 -0700

    task 7 done

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i >= j:
-            continue
-        elif i == 8 and j == 9:
-            print('{}{}'.format(i, j))
-        else:
-            print('{}{}, '.format(i, j), end="")
+def islower(c):
+    return(ord('a') <= ord(c) <= ord('z'))
