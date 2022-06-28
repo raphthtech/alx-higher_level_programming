a3aacaed582926c1c726311d144b75ed863900b5
raphthtech <raphaellthomas@gmail.com>
2022-06-28 08:59:41 -0700

    task 6 done

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,7 +4,7 @@ for i in range(0, 10):
     for j in range(0, 10):
         if i >= j:
             continue
-        else i == 8 and j == 9:
+        elif i == 8 and j == 9:
             print('{}{}'.format(i, j))
         else:
             print('{}{}, '.format(i, j), end="")
